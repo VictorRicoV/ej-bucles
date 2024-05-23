c5721a54b4dbf22cbfb18be9d8e8fb2f34669f98
Nathalia <lessricc@gmail.com>
2024-05-23 21:57:22 +0200

    vamos mejorando

--- src/js/index.js
+++ src/js/index.js
@@ -19,13 +19,28 @@ tenNumbers(arrayAddition);
 
 //- Crea una función que reciba un número e imprima por consola la tabla de multiplicar del número que ha recibido hasta el 10. Para el 4 el resultado debe ser:
 
+const multiplicationTable = number => {
+  for (let counter = 0; counter <= 10; counter++) {
+    console.log(`${number} x ${counter} = ${number * counter}`);
+  }
+};
+multiplicationTable(4);
+
+//- Crea la función inversa para que la tabla de multiplicar salga en orden inverso. Para el 4 el resultado debe ser:
+const tableMultiplication = number => {
+  for (let counter = 10; counter >= 0; counter--) {
+    console.log(`${number} x ${counter} = ${number * counter}`);
+  }
+};
+tableMultiplication(4);
+
 //- Crea una función que reciba el año actual y tu edad, la función debe imprimir:
 
 const getNumbers = (year, age) => {
   console.log(`Naciste en el año ${year - age}`);
-  for (let counter = 1; counter < 18; counter++) {
+  for (let counter = 1; counter <= 18; counter++) {
     if (counter === 1) {
-      console.log(`En el año ${counter + (year - age)} cumpliste ${counter} años`);
+      console.log(`En el año ${counter + (year - age)} cumpliste ${counter} año`);
     } else console.log(`En el año ${year - age + counter} cumpliste ${counter} años`);
   }
 };
@@ -44,14 +59,45 @@ const evenNumbers = (numberA, numberB) => {
 
 evenNumbers(2, 12);
 
-const twoArrays = (arrayA, arrayB) => {
+const twoArrays = () => {
   let arrayA = [];
   let arrayB = [];
 
-  for (let counter = 0; counter < 6; counter++) {
+  for (let counter = 0; counter < 5; counter++) {
     arrayA.push(Math.floor(Math.random() * 11));
     arrayB.push(Math.floor(Math.random() * 11));
-    if(arrayA === arrayB)
   }
-  
+  for (const number of arrayA) {
+    if (arrayB.includes(number)) {
+      console.log(`El número ${number} está en ambos arrays`);
+    }
+  }
+  console.log(arrayA, arrayB);
 };
+twoArrays();
+
+//- Crea una función que reciba un número y te diga si es primo o no. Un número primo es aquel que sólo puede dividirse por si mismo
+
+const primeNumber = number => {
+  if (number <= 1 || number % 2 === 0) {
+    console.log(`El número ${number} no es primo.`);
+    return;
+  }
+  for (let counter = 2; counter < number; counter++) {
+    if (number % counter === 0) {
+      console.log(`El número ${number} no es primo`);
+      return;
+    }
+  }
+  console.log(`El número ${number} es primo`);
+};
+primeNumber(7);
+
+//- Crea una función que reciba un array de 10 números, imprime por consola cada número, su cuadrado y su cubo en este formato:
+
+const squareAndCube = numbers => {
+  for (let counter = 0; counter < 10; counter++) {
+    console.log(`"Número: ${numbers[counter]} - Cuadrado: ${Math.pow(numbers[counter], 2)}`);
+  }
+};
+squareAndCube([1, 2, 3, 4, 5, 6, 7, 8, 9, 11]);
